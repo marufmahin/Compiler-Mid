fbe8d8835db838ed21a8f51468f5e2e5765d54b0
Md.Maruf Hossen Mahin <maruf907182@gmail.com>
2025-11-17 19:00:50 +0600

    Add files via upload

---
+++ a1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+string input;
+cin>> input;
+int state = 0;
+
+for(int i = 0; i < input.length(); i++){
+    char c = input[i];
+    if(c != '0' && c != '1'){
+        cout<< "Invalid";
+        return 0;
+    }
+
+    if(state == 0){
+        if(c == '0') state = 1;
+        else state = 0;
+    }
+
+    else if(state == 1){
+        if(c == '0') state = 1;
+        else state = 2;
+    }
+
+    else if( state == 2){
+        if(c =='0') state = 1;
+        else state = 0;
+    }
+}
+
+if(state == 2) cout<< "Accepted";
+
+else cout<< "Rejected";
+
+return 0;
+}
+
